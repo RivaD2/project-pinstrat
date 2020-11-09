aeae0b16d52970eb26be0d8d50d83af688f25a24
RivaD2 <rivkadavidowski@gmail.com>
2020-11-09 11:54:52 -0800

    connected to db and set up all middlewares

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const server = require('./lib/server');
+server.start();
+
+//Connect to Database
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...',err));
+
+    //to gid rid of deprecation warning, I used mongoose.set
+    mongoose.set('useCreateIndex', true);
