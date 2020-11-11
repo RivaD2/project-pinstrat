4e5a91c642c05588392506a732fab2bba6b44aef
thatvetdevrob <thatvetdevrob@gmail.com>
2020-11-10 19:51:40 -0500

    adding more test

--- __tests__/404.test.js
+++ __tests__/404.test.js
@@ -4,15 +4,28 @@ const express = require('express');
 const request = require('supertest');
 const supergoose = require('@code-fellows/supergoose');
 const fourOhFour = require('../lib/middleware/404');
-
+var assert = require('assert');
 const app = express()
 let router = express.Router();
 
-describe('We are testing our 404 error message', () => {
+const res = {
+  body: 'hi',
+  message: 'world'
+};
 
-  it("should go to 404 page", () => {
+
+describe('Routing', function(){
+
+  it('should go to 404 page', () => {
     expect(fourOhFour).toHaveLength(4);
 
+  });
+
+  it('should return 404 Not Found for missing resources', function() {
+    expect(fourOhFour)
+    {
+      assert.equal(res.body.message, undefined);
+    }
   });
 
 });
--- __tests__/500.test.js
+++ __tests__/500.test.js
@@ -4,15 +4,29 @@ const express = require('express');
 const request = require('supertest');
 const supergoose = require('@code-fellows/supergoose');
 const fivehundred = require('../lib/middleware/500');
+var assert = require('assert');
 
 const app = express()
 let router = express.Router();
+
+const res = {
+  body: 'hi',
+  message: 'world'
+};
+
 
 describe('We are testing our 500 error message', () => {
 
   it("Checking the specs of the error 500", () => {
     expect(fivehundred).toHaveLength(4);
 
+  });
+
+  it('should return 505 Not Found for missing resources', function() {
+    expect(fivehundred)
+    {
+      assert.equal(res.body.message, undefined);
+    }
   });
 
 });
