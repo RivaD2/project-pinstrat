47019625ae840ffddc4057aa2f269c6af58482c3
thatvetdevrob <thatvetdevrob@gmail.com>
2020-11-11 12:48:49 -0500

    all

--- __tests__/500.test.js
+++ __tests__/500.test.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const supergoose = require('@code-fellows/supergoose');
+const express = require('express');
 const fivehundred = require('../lib/middleware/500');
-const testServer = supergoose(server.app);
 
-//may need test server in this file
 describe('We are testing our 500 error message', () => {
+
   it("Checking the specs of the error 500", () => {
     expect(fivehundred).toHaveLength(4);
+
   });
 });
